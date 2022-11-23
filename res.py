b83f7423799a98dd6f063507dc52084643d52e8e
triehubov <triehubov.nikita@outlook.com>
2022-11-23 16:33:50 +0200

    Regular 1 update

--- res.py
+++ res.py
@@ -3,8 +3,12 @@ import re
 
 ipin = str(input("Input IP here\n"))
 match = re.search(r'\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?', ipin)
-
 if match is not None:
+    ipspl = (match.group()).split('.')
+    for i in ipspl:
+        if int(i) > 255:
+            print("Not Valid IP")
+            break
     print("Valid IP: ", match.group())
 else:
     print("Is not an IP")
